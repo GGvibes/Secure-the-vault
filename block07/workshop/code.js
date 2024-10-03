885796ac0513a92299a63faafd33d9f557c742ed
GGvibes <grherzog@gmail.com>
2024-10-03 08:10:03 -0500

    adding block07

---
+++ block07/workshop/code.js
@@ -0,0 +1,17 @@
+/**
+ * 1) Declare a string variable and assign it to "You have received this message because you have 
+ *      been chosen to open an important vault. Here is the secret combination:"
+ * 2) Declare and assign 3 variables, one for each number in the combination
+ * 3) Code 1 will use subtraction
+ * 4) Code 2 will use mutiplication
+ * 5) Code 3 will use addition
+ * 6) Dispaly the string and the codes to the user
+ */
+
+const userMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+let code1 = 11 - 1
+let code2 = 10 * 4
+let code3 = 20 + 19
+
+alert(userMessage + "\n" + code1 + " " + code2 + " " + code3)
